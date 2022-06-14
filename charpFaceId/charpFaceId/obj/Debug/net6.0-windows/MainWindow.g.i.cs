55b71fe75faee4b2163a656036b1ac4d5d9096de
Jonas-Pamminger <jonas.pamminger@outlook.de>
2022-06-14 13:37:52 +0200

    add my sample data

--- charpFaceId/charpFaceId/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ charpFaceId/charpFaceId/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace charpFaceId {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/charpFaceId;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/csharpFaceId;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
