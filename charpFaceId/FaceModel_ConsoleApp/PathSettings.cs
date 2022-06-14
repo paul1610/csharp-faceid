b50d6bd3da4f206518b4e0dfb5b62cbc9208f1d9
Jonas-Pamminger <jonas.pamminger@outlook.de>
2022-06-14 15:35:41 +0200

    added basic image predictions in console

---
+++ charpFaceId/FaceModel_ConsoleApp/PathSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceModel_ConsoleApp
+{
+    public class PathSettings
+    {
+        public static string TrainImageFolerPath = @"C:\Users\Jonas Pamminger\OneDrive - HTBLA Leonding\Desktop\Schule\2 Klasse\Prog_KAS\csharp-faceid\TrainData";
+        public static string TestImageFolerPath = @"C:\Users\Jonas Pamminger\OneDrive - HTBLA Leonding\Desktop\Schule\2 Klasse\Prog_KAS\csharp-faceid\TestData";
+    }
+}
