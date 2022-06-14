35e802d97740685dbb594d9c9eb8386a80cd20ff
paul1610 <57170160+paul1610@users.noreply.github.com>
2022-06-14 15:49:41 +0200

    Renamed 'charpFaceId' to  'csharpFaceId'

--- charpFaceId/FaceModel_ConsoleApp/PathSettings.cs
+++ charpFaceId/FaceModel_ConsoleApp/PathSettings.cs
@@ -8,7 +8,7 @@ namespace FaceModel_ConsoleApp
 {
     public class PathSettings
     {
-        public static string TrainImageFolerPath = @"C:\Users\Jonas Pamminger\OneDrive - HTBLA Leonding\Desktop\Schule\2 Klasse\Prog_KAS\csharp-faceid\TrainData";
-        public static string TestImageFolerPath = @"C:\Users\Jonas Pamminger\OneDrive - HTBLA Leonding\Desktop\Schule\2 Klasse\Prog_KAS\csharp-faceid\TestData";
+        public static string TrainImageFolerPath = @"C:\Users\nellp\Desktop\Schule\2DHIF - 2021-22\PR0 OO\Übungen\08_Project\csharp-faceid\TrainData";
+        public static string TestImageFolerPath = @"C:\Users\nellp\Desktop\Schule\2DHIF - 2021-22\PR0 OO\Übungen\08_Project\csharp-faceid\TestData";
     }
 }
